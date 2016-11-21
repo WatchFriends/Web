ba1c2099f6610ed422987b1fdbb19b8ae5ffdffb
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-21 12:28:32 +0100

    klassen

---
+++ backend/models/achievements.js
@@ -0,0 +1,8 @@
+const achievementsType = ("./actievementsType.js");
+
+let achievements = (function() {
+
+    let name,
+        image,
+        description;
+})();
---
+++ backend/models/achievementsType.js
@@ -0,0 +1,13 @@
+const COLORS = [
+    BRASS = "#D1A684",
+    SILVER = "#B4B8BC",
+    GOLD = "#FFCC01"
+]
+
+let actievementsType = (function() {
+
+    let name,
+        color;
+}());
+
+module.exports = actievementsType;
