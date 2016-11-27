bc3ba3ebe4c5fcf572a326f02eef1fc32fca6b86
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-27 09:25:03 +0100

    renamed to WfApp

--- app/ts/app.module.ts
+++ app/ts/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { WfHome } from "./home";
+import { WfApp } from "./app";
 
 @NgModule ({
     imports: [BrowserModule, FormsModule],
-    declarations: [WfHome],
-    bootstrap: [WfHome]
+    declarations: [WfApp],
+    bootstrap: [WfApp]
 })
 
 export class AppModule { }
--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component ({
-    selector: "Wfhome",
+    selector: "WfApp",
     template:  `<div>
                     <h1>WatchFriends</h1>
                     <p>Welkom...</p>
                 </div>`
 })
 
-export class WfHome {
+export class WfApp {
 
 }
