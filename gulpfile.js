16c08fa2fc956eee658aea068b3f9a0a37816fd9
Jasper De Sutter <jasper.desutter@gmail.com>
2016-11-28 15:34:46 +0100

    gulpfile herschreven
    typescript toegevoegd, betere uitwerking sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require("gulp"),
     uglify = require("gulp-uglify"),
     notify = require("gulp-notify"),
     concat = require("gulp-concat"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    merge = require("merge-stream");
 
 
 const PATHS = {
@@ -17,23 +18,17 @@ const PATHS = {
         SRC: './bower_components/',
         DEST: './wwwroot/lib'
     },
-    FONTS: {
-        SRC: './app/fonts/',
-        DEST: './wwwroot/fonts'
-    },
     CSS: {
         SRC: './app/css/**/*.css',
+        SASS: './app/sass/**/*.scss',
         DEST: './wwwroot/css'
-    },
-    SASS: {
-        SRC: './app/sass/**/*.scss',
-        DEST: './app/css'
     },
     HTML: {
         SRC: './wwwroot/**/*.html'
     },
     JS: {
         SRC: "./app/js/**/*.js",
+        TS: "./app/ts/**/*.ts",
         DEST: "./wwwroot/js"
     },
     NODE: {
@@ -43,13 +38,14 @@ const PATHS = {
 
 gulp.task("default", function () {
     var htmlWatcher = gulp.watch(PATHS.HTML.SRC, ['html-validate']),
-        sassWatcher = gulp.watch(PATHS.SASS.SRC, ['sass']),
         cssWatcher = gulp.watch(PATHS.CSS.SRC, ['css']),
+        cssWatcher = gulp.watch(PATHS.CSS.SASS, ['css']),
         jsWachter = gulp.watch(PATHS.JS.SRC, ['js']),
+        tsWachter = gulp.watch(PATHS.JS.TS, ['js']),
         nodeWachter = gulp.watch(PATHS.NODE.SRC, ['node']);
         
     cssWatcher.on('change', function (event) {
-       console.log("File: " + event.path + " was " + event.type);
+       console.log("File '" + event.path + "' was " + event.type);
     });
 });
 
@@ -58,35 +54,38 @@ const AUTOPREFIXOPTIONS = {
 };
 
 gulp.task("css", function () {
-    gulp.src(PATHS.CSS.SRC)
+    var css = gulp.src(PATHS.CSS.SRC)
         .pipe(sourcemaps.init())
         .pipe(autoprefixer(AUTOPREFIXOPTIONS))
         .pipe(csslint())
-        //.pipe(csslint.formatter())
+        .pipe(csslint.formatter())
         .pipe(concat("main.min.css"))
         .pipe(cleanCSS({debug: true, compatibility: '*'}, function (details) {
-            //console.log(details);
-            console.log(details.name + ": " + details.stats.originalSize);
-            console.log(details.name + ": " + details.stats.minifiedSize);
+            console.log(details.name + ": " + (details.stats.minifiedSize - details.stats.originalSize));
         }))
         .pipe(sourcemaps.write())
+    var scss = gulp.src(PATHS.CSS.SASS)
+      .pipe(sourcemaps.init())
+      .pipe(sass.on('error',sass.logError))
+      .pipe(sourcemaps.write());
+    return merge(css,sass)
+        .pipe(concat("main.min.css"))
         .pipe(gulp.dest(PATHS.CSS.DEST));
 });
 
-gulp.task("sass", function () {
-    gulp.src(PATHS.SASS.SRC)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(PATHS.SASS.DEST));
-});
-
 gulp.task("js", function () {
-    gulp.src(PATHS.JS.SRC)
+    var js = gulp.src(PATHS.JS.SRC)
         .pipe(jshint())
         .pipe(jshint.reporter(jsStylish))
         .pipe(sourcemaps.init())
-        .pipe(concat("app.min.js"))
+        .pipe(uglify())
+        .pipe(sourcemaps.write());
+    var ts = gulp.src(PATHS.JS.TS)
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write())
+    return merge(js, ts)
+        .pipe(concat("app.min.js"))
         .pipe(gulp.dest(PATHS.JS.DEST))
         .pipe(notify({message: 'js built'}));
 });
@@ -108,9 +107,4 @@ gulp.task("copy-externals", function () {
     // dist folder van bower_components nr lib in wwwroot kopieren
     gulp.src(PATHS.EXTERNALS.SRC + "bootstrap/dist/**")
         .pipe(gulp.dest(PATHS.EXTERNALS.DEST + "/bootstrap"))
-});
-
-gulp.task("copy-fonts", function () {
-    gulp.src(PATHS.FONTS.SRC + "*")
-        .pipe(gulp.dest(PATHS.FONTS.DEST));
 });
