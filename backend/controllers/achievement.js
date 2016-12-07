cec88a0974144def9e81f4ff41ca22a051d80174
Jasper De Sutter <jasper.de.sutter@student.howest.be>
2016-12-07 14:17:15 +0100

    fix achievement controller

--- backend/controllers/achievement.js
+++ backend/controllers/achievement.js
@@ -2,9 +2,10 @@ var service = require("./../data/databaseService.js"),
     express = require("express"),
     router = express.Router();
 
-router.get("achievement/", (res, req, next) => service.get(req.params.id, (err, data) => {
-    if(err) next(err);
-    else res.json(data);
-}));
+router.get("/achievement", (req, res, next) =>
+    service.getAchievements((err, data) => {
+        if(err) next(err);
+        else res.json(data);
+    }));
 
 module.exports = router;
