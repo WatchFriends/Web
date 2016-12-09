61076428a224f538ec01ee323437f3ae225c6fae
Jasper De Sutter <jasper.de.sutter@student.howest.be>
2016-12-09 16:22:37 +0100

    testcomponent weg

--- app/ts/app.module.ts
+++ app/ts/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { TestComponent} from "./test.component";
 
 @NgModule ({
     declarations: [
-        AppComponent,
-        TestComponent
+        AppComponent
     ],
     imports: [
         BrowserModule, 
