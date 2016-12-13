633b694e480345ff8bba1b1fd00d6bf6880ef993
Jasper De Sutter <jasper.desutter@gmail.com>
2016-12-13 20:59:59 +0100

    config

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,38 @@ var webpack = require("webpack"),
     path = require("path"),
     merge = require("webpack-merge"),
     {CheckerPlugin} = require('awesome-typescript-loader');
-    
+
 module.exports = {
-    entry:  "./app/ts/main",
+    target: 'web',
+    entry: "./app/ts/main",
     output: {
         path: "./wwwroot",
         filename: "app.bundle.js"
     },
+    devtool: "inline-source-map",
     module: {
-        loaders: [
-            {test: /\.ts$/, loaders: [
-                //"ts",
-                "awesome-typescript",
-                "angular2-template",
-                //"angular2-router"
-            ]},
-        { test: /\.(css|html)$/, loader: "raw"}
+        rules: [
+            {
+                test: /\.ts$/, loaders: [
+                    //"ts",
+                    "awesome-typescript",
+                    "angular2-template",
+                    //"angular2-router"
+                ]
+            },
+            { test: /\.html$/, loader: "raw" },
+            { test: /\.css$/, loaders: ["raw", "postcss"] },
+            { test: /\.scss$/, loaders: ["raw", "postcss", "sass"] },
+            { test: /\.json$/, loader: 'json' },
         ]
     },
     plugins: [
         new CheckerPlugin(),
     ],
     resolve: {
-        extensions: [".ts",".js"],
-        root: [
-            path.resolve("./node_modules")
+        extensions: [".ts", ".js", ".json"],
+        modules: [
+            path.join(__dirname, "node_modules")
         ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        watchOptions: { aggregateTimeout: 300, poll: 1000 }
-    },
-    node: {
-        global: true,
-        crypto: 'empty',
-        __dirname: true,
-        __filename: true,
-        process: true,
-        Buffer: false,
-        clearImmediate: false,
-        setImmediate: false
     }
 }
