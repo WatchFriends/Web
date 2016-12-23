c706b2e71924c8592eca66674cd91272356ac6df
Jasper De Sutter <jasper.desutter@gmail.com>
2016-12-23 16:02:42 +0100

    selectors weg

--- frontend/app/register/register.component.ts
+++ frontend/app/register/register.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'register-form',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
