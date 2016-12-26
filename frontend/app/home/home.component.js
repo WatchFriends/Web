5744597e4d33b76982b0fc8dc726f48d95a22728
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-12-26 15:32:17 +0100

    10 van 11 #88

--- frontend/app/home/home.component.js
+++ frontend/app/home/home.component.js
@@ -1,21 +1,28 @@
 "use strict";
 
 (function() {
-    var images = [],
-        body,
-        height,
-        position = 0;
+    var containers = [],
+        imagewrappers = [],
+        body, height, width, position = 0;
 
     function init() {
-        images =  document.querySelectorAll(".imagecontainer, .imagecontainer_wrapper, .imagecontainer_content, .contentcontainer");
+        containers = document.querySelectorAll(".imagecontainer, .imagecontainer_wrapper, .imagecontainer_content, .contentcontainer");
+        imagewrappers = document.querySelectorAll(".imagecontainer_wrapper");
         body = document.body;
         height = window.innerHeight;
+        width = window.innerWidth;
         //document.getElementsByClassName("carousel").carousel();
     }
 
     function setHeight() {
-        for (var i = images.length; i--;) {
-            images[i].style.height = height + "px";
+        for (var i = containers.length; i--;) {
+            containers[i].style.height = height + "px";
+        }
+
+        if (parseInt(width) <= 992) {
+            for (var i = imagewrappers.length; i--;) {
+                imagewrappers[i].style.width = width + "px";
+            }
         }
     }
 
