607304b6b15a50ee8aa3f8ffba6dcdf81ef5321c
michielzyde <michiel.zyde96@gmail.com>
2016-12-27 16:52:46 +0100

    Bug wazegheid gefixt + zichtbare titels

--- frontend/app/explore/explore.component.ts
+++ frontend/app/explore/explore.component.ts
@@ -7,15 +7,15 @@ import { Component} from '@angular/core';
 })
 export class ExploreComponent  {
 
-   imageSerieBigLeft: string = "http://www.desktopimages.org/pictures/2014/0309/1/orig_60277.jpg";
+   imageSerieBigLeft: string = "http://fanaru.com/suits/image/6805-suits-suits.jpg";
    imageSerieBigRight: string = "http://www.araspot.com/wp-content/uploads/2016/01/second-chance-season-1-wallpaper-desktop-background-j0md1g8q0w.jpg";
    imageSerieRow: string = "http://wallpapersdsc.net/wp-content/uploads/2015/11/286.jpg";
    imageSerieRowSecond: string =  "http://hdwallpaperbackgrounds.net/wp-content/uploads/2015/10/Game-Of-Thrones-Emilia-Clarke-With-Dragon-on-Shoulder-Wallpaper-1920x1080.jpg";
-   imageSerieRowThird: string = "https://images8.alphacoders.com/417/417024.jpg";
+   imageSerieRowThird: string = "http://wallpapercave.com/wp/DXq0duZ.jpg";
    imageSerieRowFourth: string = "http://wallpapercave.com/wp/U4sY2La.jpg";
    imageSerieSecondRow: string = "https://wallpaperscraft.com/image/dexter_michael_c_hall_polyethylene_99093_1920x1080.jpg";
    imageSerieSecondRowSecond: string = "http://www.thewallpapers.org/photo/77346/Silicon-Valley-002.jpg";
-   imageSerieSecondRowThird: string = "https://s4.scoopwhoop.com/anj/15TVshows3/101064750.jpg";
+   imageSerieSecondRowThird: string = "http://www.pixelstalk.net/wp-content/uploads/2016/07/The-100-TV-Series-Wallpapers-HD.jpg";
    imageSerieSecondRowFourth: string = "http://eskipaper.com/images/breaking-bad-wallpaper-3.jpg";
    //images: string[] = ["http://www.desktopimages.org/pictures/2014/0309/1/orig_60277.jpg", "http://www.araspot.com/wp-content/uploads/2016/01/second-chance-season-1-wallpaper-desktop-background-j0md1g8q0w.jpg"];
 
