f485e05d2520d2b4c4ec9b465dee94d4ab451293
michielzyde <michiel.zyde96@gmail.com>
2016-12-28 20:32:48 +0100

    Profile info completed & responsive

--- frontend/app/profile/profile.component.ts
+++ frontend/app/profile/profile.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
-  backgroundImage: string = "http://wallpaperrs.com/uploads/creative-graphics/vintage-pattern-wide-wallpaper-30183.jpg";
+  backgroundProfile: string = "http://wallpaperpawn.us/wp-content/uploads/2016/07/royal-wall-paper-minimalistic-pink-patterns-damask-royal-simple-wallpapers.jpg";
   profilePicture: string = "https://lh4.googleusercontent.com/-6AAQyF6tbRM/AAAAAAAAAAI/AAAAAAAAJZ0/BEAm-nmN4a4/photo.jpg";
+  backgroundWatchlist: string = "http://wallpapersinsider.com/wp-content/uploads/2016/03/TV-Series-Killjoys-Wallpapers.jpg";
 
 }
