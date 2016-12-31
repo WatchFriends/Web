4b596a8065d4bad3ab5e94cb754580328a49a031
michielzyde <michiel.zyde96@gmail.com>
2016-12-31 10:03:47 +0100

    Profile Page
    Profiel pagina met de watchlist van die persoon & achievements

--- frontend/app/profile/profile.component.ts
+++ frontend/app/profile/profile.component.ts
@@ -8,7 +8,11 @@ import { Component } from '@angular/core';
 export class ProfileComponent {
 
   backgroundProfile: string = "http://wallpaperpawn.us/wp-content/uploads/2016/07/royal-wall-paper-minimalistic-pink-patterns-damask-royal-simple-wallpapers.jpg";
-  profilePicture: string = "https://lh4.googleusercontent.com/-6AAQyF6tbRM/AAAAAAAAAAI/AAAAAAAAJZ0/BEAm-nmN4a4/photo.jpg";
-  backgroundWatchlist: string = "http://wallpapersinsider.com/wp-content/uploads/2016/03/TV-Series-Killjoys-Wallpapers.jpg";
-
+  profilePicture: string = "https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/14051786_1146069705449340_95700626649935794_n.jpg?oh=04be87d50b50a66ce9b42022df8b2fe5&oe=58E04019";
+  imgWatchlistFirst: string = "http://wallpapersqq.net/wp-content/uploads/2015/11/Zoo-tv-series-6.jpg";
+  imgWatchlistSecond: string = "https://wallpaperscraft.com/image/vikings_tv_series_historical_drama_katheryn_winnick_lagertha_sword_shield_nature_94816_1920x1080.jpg";
+  imgWatchlistThird: string = "http://wallpapersdsc.net/wp-content/uploads/2016/08/Salem-Wallpapers.jpg";
+  imgWatchlistSecondRow: string = "http://wallpapersinsider.com/wp-content/uploads/2016/04/Preacher-TV-Series-Wallpapers.jpg";
+  imgWatchlistSecondRowSecond: string = "http://www.wallpapermade.com/images/wallpapers/originals/gotham-characters-in-a-dark-street-wallpaper-3476.jpg";
+  imgWatchlistSecondRowThird: string = "http://cdn.wallpapersafari.com/87/70/jywP9k.jpg";
 }
