feeaa80d4d9e309271912df84086bc96967632b6
Michiel Vermeersch <mick.vermeersch@gmail.com>
2017-01-02 16:50:45 +0100

    removed versionkey

--- backend/models/followedSerie.js
+++ backend/models/followedSerie.js
@@ -6,9 +6,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var followedSeriesSchema = new Schema({
-    userId: String,
-    seriesId: Number,
-    following: Boolean
-});
+        userId: String,
+        seriesId: Number,
+        following: Boolean
+    }, {versionKey: false}
+);
 
 module.exports = mongoose.model('followedSeries', followedSeriesSchema);
