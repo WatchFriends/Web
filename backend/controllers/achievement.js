3acfd22850f5f805423e4e6f22c39668350dd530
michielzyde <michiel.zyde96@gmail.com>
2017-01-05 19:03:20 +0100

    Changes

--- backend/controllers/achievement.js
+++ backend/controllers/achievement.js
@@ -1,11 +1,14 @@
-var service = require("./../data/databaseService.js"),
-    express = require("express"),
-    router = express.Router();
+const service = require("./../data/databaseService.js"),
+      express = require("express"),
+      router = express.Router();
 
 router.get("/achievement", (req, res, next) => {
     service.getAchievements((err, data) => {
         if(err) {
             next(err);
+        }
+        else if (data === null) {
+            next(new Error("Our service is temporarily unavailable"));
         }
         else {
             res.json(data);
