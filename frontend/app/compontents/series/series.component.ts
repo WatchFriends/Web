bf225613eadd099a159886be632c9fd74a766104
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2017-01-07 18:02:47 +0100

    #155 met jade :smile:

---
+++ frontend/app/compontents/series/series.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './series.component.jade',
+    styleUrls: ['./series.component.scss'],
+    selector: 'wfseries'
+})
+
+export class Wfseries {
+
+}
