d18d54501fddb08a343768e042ad5d28f9d795e1
Michiel Vermeersch <mick.vermeersch@gmail.com>
2017-01-08 16:33:49 +0100

    Event sort

---
+++ backend/controllers/userEvents.js
@@ -0,0 +1,45 @@
+const apiService = require('./../data/apiService'),
+    dbService = require('./../data/databaseService'),
+    express = require('express'),
+    router = express.Router(),
+    errors = require('../helpers/errors'),
+    async = require('async'),
+    followedSeries = require('../models/followedSeries');
+
+const callback = (res, next) =>
+    (err, data) => {
+        if (err) {
+            return next(err);
+        }
+        res.json(data);
+    };
+
+router.post('/event', (req, res, next) => {
+    dbService.addEvent(req.body, req.user, callback(res, next));
+});
+
+router.get('/feed', (req, res, next) => {
+    //Get all users friends
+    apiService.request(`user/${req.user._id}/follows`, (err, user) => {
+        if (err) return next(err);
+        let feed = [];
+        //Get all activity by those friends
+        async.each(user, (item, cb) => {
+            apiService.request(`user/${item._id}/events`, (err, events) => {
+                if (err) return cb(err);
+                feed.push(events);
+            });
+            cb();
+        }, err => {
+            if (err) return next(err);
+            feed.sort(function (a, b) {
+                a = new Date(a.time);
+                b = new Date(b.time);
+                return a > b ? -1 : a < b ? 1 : 0;
+            });
+            res.json(feed);
+        });
+    });
+});
+
+module.exports = router;
