c5ed8f003fd8af5e2ff11d47bae31e13f03993f7
Michiel Vermeersch <mick.vermeersch@gmail.com>
2017-01-17 17:50:20 +0100

    dbService cleanup

--- backend/data/databaseService.js
+++ backend/data/databaseService.js
@@ -8,7 +8,48 @@ const config = require('./config.json'),
     watchedEpisode = require('../models/watchedEpisode'),
     wfevent = require('../models/wfevent');
 
-let existsWatchedEpisode = (body, cb) => {
+module.exports = {
+    /* ACHIEVEMENTS */
+    getAchievements: (cb) => achievement.find({}).exec(cb),
+
+    /* FOLLOWEDSERIES */
+    getFollowedSeries: (userId, cb) =>
+        followedSeries.find({userId, following: true}, {_id: 0, user: 0}).exec(cb),
+
+    updateFollowedSeries: (userId, seriesId, data, cb) =>
+        followedSeries.update({userId, seriesId}, data, {upsert: true, setDefaultsOnInsert: true}).exec(cb),
+
+    findFollowedSeries: (userId, seriesId, cb) =>
+        followedSeries.findOne({userId, seriesId}, {_id: 0, user: 0, seriesId: 0}).exec(cb),
+
+    addFollowedSeries: (userId, series, cb) => {
+        followedSeries.findOne({userId, seriesId: series.id}, {following: 1, rating: 1})
+            .exec((err, followed) => {
+
+                if (err) return cb(err);
+
+                series["following"] = followed ? followed.following : false;
+                series["rating"] = followed ? followed.rating : -1;
+
+                cb(null, series);
+            });
+    },
+
+    addFollowedSeriesList: (userId, seriesList, cb) => {
+        let results = [];
+        async.each(seriesList, (item, cb) =>
+            module.exports.addFollowedSeries(userId, item, (err, series) => {
+                if (err) return cb(err);
+                results.push(series);
+                cb();
+            }), err => {
+            if (err) return cb(err);
+            cb(null, results);
+        });
+    },
+
+    /* EPISODE */
+    existsWatchedEpisode: (body, cb) => {
         watchedEpisode
             .count({
                 userId: body.userId,
@@ -18,8 +59,8 @@ let existsWatchedEpisode = (body, cb) => {
             })
             .exec(cb);
     },
-    updateWatchedEpisode = (body, cb) => {
-        existsWatchedEpisode(body, (err, count) => {
+    updateWatchedEpisode: (body, cb) => {
+        module.exports.existsWatchedEpisode(body, (err, count) => {
             if (count > 0) {
                 watchedEpisode.update({
                         userId: body.userId,
@@ -45,7 +86,7 @@ let existsWatchedEpisode = (body, cb) => {
             }
         });
     },
-    getWatchedEpisodesBySeriesSeasonId = (params, user, cb) => {
+    getWatchedEpisodesBySeriesSeasonId: (params, user, cb) => {
         watchedEpisode.find({
             userId: user._id,
             seriesId: params.series,
@@ -55,126 +96,8 @@ let existsWatchedEpisode = (body, cb) => {
             watched: 0,
             __v: 0,
         }).exec(cb);
-    };
-
-function addFollowedSeries(userId, series, cb) {
-    followedSeries.findOne({userId, seriesId: series.id}, {following: 1, rating: 1})
-        .exec((err, followed) => {
-
-            if (err) return cb(err);
-
-            series["following"] = followed ? followed.following : false;
-            series["rating"] = followed ? followed.rating : -1;
-
-            cb(null, series);
-        });
-}
-
-function addWFEvent(body, user, cb) {
-    let username = user.name.givenName + ' ' + user.name.familyName;
-    let friendname;
-    if (body.friend !== undefined) {
-        friendname = body.friend.name.givenName + ' ' + body.friend.name.familyName;
-    }
-    let newEvent = new wfevent({
-        userId: user._id,
-        userName: username,
-        message: '',
-        url: ''
-    });
-    let err = new Error('Provided parameters do not form a correct event!');
-
-    if (body.friendId !== undefined && body.friend !== undefined) {
-        if (body.following !== undefined) {
-            switch (body.following.toString()) {
-                case 'true' || true:
-                    newEvent.message = ' is now following ' + friendname + '.';
-                    break;
-                case 'false' || false:
-                    newEvent.message = ' is no longer following ' + friendname + '.';
-                    break;
-            }
-            newEvent.url = '/profile/' + body.friend.id;
-            newEvent.save(cb);
-        } else {
-            cb(err);
-        }
-    } else if (body.seriesId !== undefined && body.seriesName) {
-        if (body.seasonId !== undefined && body.watched !== undefined) {
-            if (body.episodeId !== undefined) {
-                switch (body.watched.toString()) {
-                    case 'true' || true:
-                        newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' episode ' + body.episodeId + ' as watched.';
-                        break;
-                    case 'false' || false:
-                        newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' episode ' + body.episodeId + ' as not longer watched.';
-                        break;
-                }
-                newEvent.url = '/series/' + body.seriesId + '/season/' + body.seasonId + '/episode/' + body.episodeId;
-                newEvent.save(cb);
-            } else {
-                switch (body.watched.toString()) {
-                    case 'true' || true:
-                        newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' as watched.';
-                        break;
-                    case 'false' || false:
-                        newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' as no longer watched.';
-                        break;
-                }
-                newEvent.url = '/series/' + body.seriesId;
-                newEvent.save(cb);
-            }
-        } else if (body.following !== undefined) {
-            switch (body.following.toString()) {
-                case 'true':
-                    newEvent.message = ' is now following ' + body.seriesName + '.';
-                    break;
-                case 'false':
-                    newEvent.message = ' is no longer following ' + body.seriesName + '.';
-                    break;
-            }
-            newEvent.url = '/series/' + body.seriesId;
-            newEvent.save(cb);
-        } else {
-            cb(err);
-        }
-    } else {
-        cb(err)
-    }
-}
-
-function getWFEventsByUserId(userId, cb) {
-    wfevent.find({userId: userId}).exec(cb);
-}
-
-module.exports = {
-    /* ACHIEVEMENTS */
-    getAchievements: (cb) => achievement.find({}).exec(cb),
-
-    /* FOLLOWEDSERIES */
-    getFollowedSeries: (userId, cb) =>
-        followedSeries.find({userId, following: true}, {_id: 0, user: 0}).exec(cb),
-
-    updateFollowedSeries: (userId, seriesId, data, cb) =>
-        followedSeries.update({userId, seriesId}, data, {upsert: true, setDefaultsOnInsert: true}).exec(cb),
-
-    findFollowedSeries: (userId, seriesId, cb) =>
-        followedSeries.findOne({userId, seriesId}, {_id: 0, user: 0, seriesId: 0}).exec(cb),
-
-    addFollowedSeries,
-
-    addFollowedSeriesList: (userId, seriesList, cb) => {
-        let results = [];
-        async.each(seriesList, (item, cb) =>
-            addFollowedSeries(userId, item, (err, series) => {
-                if (err) return cb(err);
-                results.push(series);
-                cb();
-            }), err => {
-            if (err) return cb(err);
-            cb(null, results);
-        });
     },
+
     /* USER */
     getUser: (id, cb) =>
         user.findById(id, {name: 1, email: 1, _id: 1}).exec(cb),
@@ -183,7 +106,6 @@ module.exports = {
         user.find({$text: {$search: query}}).exec(cb),
 
     /* FOLLOWER */
-
     getFollowers: (userId, cb) =>
         follower.find({userId}).exec(cb),
 
@@ -205,10 +127,80 @@ module.exports = {
         follower.find({userId: followsId, followedId: userId}).remove().exec(cb);
     },
 
-    /* WATCHEDEPISODE */
-    findWatchedEpisode: existsWatchedEpisode,
-    updateWatchedEpisode,
-    getWatchedEpisodesBySeriesSeasonId,
-    addWFEvent,
-    getWFEventsByUserId
+    /* FEED */
+    addWFEvent: (body, user, cb) => {
+        let username = user.name.givenName + ' ' + user.name.familyName;
+        let friendname;
+        if (body.friend !== undefined) {
+            friendname = body.friend.name.givenName + ' ' + body.friend.name.familyName;
+        }
+        let newEvent = new wfevent({
+            userId: user._id,
+            userName: username,
+            message: '',
+            url: ''
+        });
+        let err = new Error('Provided parameters do not form a correct event!');
+
+        if (body.friendId !== undefined && body.friend !== undefined) {
+            if (body.following !== undefined) {
+                switch (body.following.toString()) {
+                    case 'true' || true:
+                        newEvent.message = ' is now following ' + friendname + '.';
+                        break;
+                    case 'false' || false:
+                        newEvent.message = ' is no longer following ' + friendname + '.';
+                        break;
+                }
+                newEvent.url = '/profile/' + body.friend.id;
+                newEvent.save(cb);
+            } else {
+                cb(err);
+            }
+        } else if (body.seriesId !== undefined && body.seriesName) {
+            if (body.seasonId !== undefined && body.watched !== undefined) {
+                if (body.episodeId !== undefined) {
+                    switch (body.watched.toString()) {
+                        case 'true' || true:
+                            newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' episode ' + body.episodeId + ' as watched.';
+                            break;
+                        case 'false' || false:
+                            newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' episode ' + body.episodeId + ' as not longer watched.';
+                            break;
+                    }
+                    newEvent.url = '/series/' + body.seriesId + '/season/' + body.seasonId + '/episode/' + body.episodeId;
+                    newEvent.save(cb);
+                } else {
+                    switch (body.watched.toString()) {
+                        case 'true' || true:
+                            newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' as watched.';
+                            break;
+                        case 'false' || false:
+                            newEvent.message = ' has marked ' + body.seriesName + ' season ' + body.seasonId + ' as no longer watched.';
+                            break;
+                    }
+                    newEvent.url = '/series/' + body.seriesId;
+                    newEvent.save(cb);
+                }
+            } else if (body.following !== undefined) {
+                switch (body.following.toString()) {
+                    case 'true':
+                        newEvent.message = ' is now following ' + body.seriesName + '.';
+                        break;
+                    case 'false':
+                        newEvent.message = ' is no longer following ' + body.seriesName + '.';
+                        break;
+                }
+                newEvent.url = '/series/' + body.seriesId;
+                newEvent.save(cb);
+            } else {
+                cb(err);
+            }
+        } else {
+            cb(err)
+        }
+    },
+    getWFEventsByUserId: (userId, cb) => {
+        wfevent.find({userId: userId}).exec(cb);
+    }
 };
