52671cb005a5598332e7919b13d00b29a8b26ac0
Michiel Vermeersch <mick.vermeersch@gmail.com>
2017-01-17 21:52:07 +0100

    travis fix2?

--- frontend/app/services/api.service.ts
+++ frontend/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions} from '@angular/http';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
 import {Observable} from 'rxjs';
 import {ServerError} from './server-error';
 import {UserService} from './user.service';
