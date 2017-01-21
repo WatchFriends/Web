b33c5f56438679f4389b9ef6e309fe9ecb082660
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2017-01-21 18:24:12 +0100

    8 van 18 #198

--- frontend/app/home/home.component.ts
+++ frontend/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { UserService } from '../services';
 
 @Component({
@@ -7,14 +7,14 @@ import { UserService } from '../services';
     encapsulation: ViewEncapsulation.None
 })
 
-export class HomeComponent{
+export class HomeComponent {
 
     constructor(public user: UserService) { }
 
     imageWelcome: string = 'http://www.siwallpaperhd.com/wp-content/uploads/2016/03/tv_series_viking_background_wallpaper.jpg';
     imageSeries: string = 'http://cdn.pcwallart.com/images/arrow-tv-series-wallpaper-2.jpg';
     imageFollowers: string = 'http://justgoodvibe.com/wp-content/uploads/2015/12/gotham-1.jpg';
-    logoPng : string = '../../assets/WatchFriends_Logo_Blue.png';
+    logoPng: string = '../../assets/WatchFriends_Logo_Blue.png';
     androidApp: string = '../../assets/android_app2.png';
     playStoreButton: string = '../../assets/google_play_store_btn.png';
 
